6ebbf895e2e021e8e72b7f62f52e4344b12c7981
Frantisek Havluj <moskyt@rozhled.cz>
2012-10-15 00:42:32 +0200

    after migration…..

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20121013113324) do
+ActiveRecord::Schema.define(:version => 20121014223300) do
 
   create_table "events", :force => true do |t|
     t.string   "name"
@@ -27,6 +27,7 @@ ActiveRecord::Schema.define(:version => 20121013113324) do
     t.string   "place"
     t.string   "rozpis_url"
     t.string   "pokyny_url"
+    t.date     "deadline"
   end
 
 end
